ac1e82a18ca9a3150e8f0a7b79112e90abad38aa
JyothiPeddireddy <jyothipeddireddy75@gmail.com>
2026-09-12 21:23:14 +0530

    Added updated files

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://127.0.0.1:5173",
         "https://budget-buddy-ten-theta.vercel.app",
     ],
     allow_credentials=True,
